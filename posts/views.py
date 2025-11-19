2a6b8b89fd9f49754400644553526b7cb508f0b4
Роман <romankits08@gmail.com>
2025-11-19 14:08:50 +0200

    Add Detail post page, updated reaction login and other global changes

--- posts/views.py
+++ posts/views.py
@@ -5,14 +5,15 @@ from django.urls import reverse_lazy
 from posts.models import Post, Like
 from posts.forms import *
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 
 User = get_user_model()
 
-class PostsMainView(LoginRequiredMixin, View):
+class PostsListView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_at')
         form = PostForm()
-        return render(request, 'posts/main.html', {
+        return render(request, 'posts/posts_list.html', {
             'posts': posts,
             'form': form
         })
@@ -23,21 +24,44 @@ class PostsMainView(LoginRequiredMixin, View):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('main')
+            return redirect('posts-list')
 
         posts = Post.objects.all().order_by('-created_at')
-        return render(request, 'posts/main.html', {
+        return render(request, 'posts/posts_list.html', {
             'posts': posts,
             'form': form
         })
     
-class ToggleLikeView(LoginRequiredMixin, View):
-    def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+class ToggleLikeAPI(LoginRequiredMixin, View):
+    def post(self, request, post_pk):
+        post = get_object_or_404(Post, pk=post_pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             like.delete()
+            liked = False
+        else:
+            liked = True
 
-        return redirect('main')
+        return JsonResponse({
+            'liked': liked,
+            'likes_count': post.likes.count()
+        })
 
+class PostDetailView(LoginRequiredMixin, View):
+    def get(self, request, post_pk):
+        post = get_object_or_404(Post, pk=post_pk)
+        like = Like.objects.filter(post=post)
+
+        viewed = post.viewers.filter(id=request.user.id).exists()
+        if not viewed:
+            post.viewers.add(request.user)
+
+        viewers_count = post.viewers.count()
+        liked_by_user = like.filter(user=request.user).exists()
+            
+        return render(request, 'posts/post_detail.html', {
+            'post': post,
+            'viewers_count': viewers_count,
+            'liked_by_user': liked_by_user
+        })
