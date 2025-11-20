fbe397b6b2f47bff410936379f7d44e0941e1089
Роман <romankits08@gmail.com>
2025-11-20 16:55:56 +0200

    Add messenger models and system, views, template from last project. Changed name of channels to groups through conflicts

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -6,11 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
 import os
-
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import messenger.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            messenger.routing.websocket_urlpatterns
+        )
+    ),
+})
+
