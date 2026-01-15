1f02e9c80d94e87d416964fe109cb748098c667b
Rokit8481 <romankits08@gmail.com>
2026-01-15 18:00:19 +0200

    fixing bugs, add whitenoise to requirements

--- social_network/settings.py
+++ social_network/settings.py
@@ -143,9 +143,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Login configuration
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
